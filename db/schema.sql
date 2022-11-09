c46bb87fa8f7836107a44b6c6e5b16fbcae46b96
Jules Joseph <julesj93@gmail.com>
2022-11-08 21:54:17 -0500

    editing schema db

--- db/schema.sql
+++ db/schema.sql
@@ -4,33 +4,28 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
-CREATE TABLE courses (
+CREATE TABLE category (
   id INT NOT NULL,
-  course_title VARCHAR(30) NOT NULL,
-  course_description TEXT NOT NULL,
-  active BOOLEAN NOT NULL,
-  date_added DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+  category_name STRING NOT NULL,
+  course_description TEXT NOT NULL
 );
 
-CREATE TABLE students (
+CREATE TABLE product (
   id INT NOT NULL,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  active BOOLEAN NOT NULL,
-  date_updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+  product_name STRING VARCHAR(30) NOT NULL,
+  price VARCHAR(30) NOT NULL,
+  stock BOOLEAN NOT NULL,
+  category_id DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-CREATE TABLE instructors (
+CREATE TABLE tag (
   id INT NOT NULL,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  active BOOLEAN NOT NULL,
-  date_updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+  tag_name STRING
 );
 
-CREATE TABLE classrooms (
+CREATE TABLE productTag (
   id INT NOT NULL,
-  building_name VARCHAR(30) NOT NULL,
+  product_id VARCHAR(30) NOT NULL,
   room_number INT NOT NULL,
   available BOOLEAN NOT NULL,
   date_updated DATETIME NOT NULL
