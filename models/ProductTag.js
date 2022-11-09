0d579c18abb739b41abb723be89a32b3dadd6162
Jules Joseph <julesj93@gmail.com>
2022-11-08 22:56:20 -0500

    added product id tag

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -12,6 +12,12 @@ ProductTag.init(
       primaryKey: true,
       autoIncrement: true
       },
+    product_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'product',
+          key: 'id'
+        }
   {
     sequelize,
     timestamps: false,
