2571a075adad0217f4d8fc3b1d70ed36e421e158
Jules Joseph <julesj93@gmail.com>
2022-11-08 23:24:09 -0500

    restuctured index.js to account for each of the tables created in the db

--- models/index.js
+++ models/index.js
@@ -5,20 +5,6 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
-
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
-// Products belongsTo Category
 product.belongs(Category, {
   foreignKey: 'category_id',
 });
@@ -34,6 +20,20 @@ Product.belongsToMany(Tag, {
   foreignKey: 'product_id',
 });
 
-LibraryCard.belongsTo(Reader, {
-  foreignKey: 'reader_id',
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through:ProductTag,
+  //as: 'product_tag',
+  foreignKey: 'tag_id',
+ 
+  
 });
+
+
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
