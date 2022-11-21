903ae844ecbecc7cbc6a3788cd476e974129db26
Jules Joseph <julesj93@gmail.com>
2022-11-21 01:06:20 -0500

    one more change to index to get port to listen on 3001

--- models/index.js
+++ models/index.js
@@ -5,20 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-product.belongs(Category, {
+Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
+
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
 });
+
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  foreignKey: 'reader_id',
   through:ProductTag,
  // as: "product_tag"
   foreignKey: 'product_id',
+ 
+  
 });
+
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
@@ -28,6 +32,8 @@ Tag.belongsToMany(Product, {
  
   
 });
+
+
 
 module.exports = {
   Product,
