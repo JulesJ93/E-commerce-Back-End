649921c6e5374c51e7c8f4f04b728249684cf15c
Jules Joseph <julesj93@gmail.com>
2022-11-21 00:52:12 -0500

    added route for tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,13 +6,48 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: {
+      model: Product,
+      attributes: ["id" , 'product_name', "price", "stock" , "category_id"],
+    },
+  })
+  .then((dbTagData) => {
+    if (!dbTagData) {
+      res.status(404).json({ message: "No Categories found with this " });
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
-
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where:{
+      id: req.params.id,
+    },
+    include: {
+      model: Product,
+      attributes: ['id' , 'product_name' , 'price' , 'stock','category_id'],
+    }
+  })
+  .then((dbTagData) => {
+    if (!dbTagData) {
+      res.status(404).json({ message: "No Categories found" });
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
-
 router.post('/', (req, res) => {
   // create a new tag
 });
