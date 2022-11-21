5fa73dbeb0b6d8a9623d4b7abfd71fb9afc733e1
Jules Joseph <julesj93@gmail.com>
2022-11-21 00:58:09 -0500

    fixing seeds for schema database

--- seeds/index.js
+++ seeds/index.js
@@ -1,23 +1,26 @@
-const { Category } = require('../models');
+const seedCategories = require('./category-seeds');
+const seedProducts = require('./product-seeds');
+const seedTags = require('./tag-seeds');
+const seedProductTags = require('./product-tag-seeds');
 
-const categoryData = [
-  {
-    category_name: 'Shirts',
-  },
-  {
-    category_name: 'Shorts',
-  },
-  {
-    category_name: 'Music',
-  },
-  {
-    category_name: 'Hats',
-  },
-  {
-    category_name: 'Shoes',
-  },
-];
+const sequelize = require('../config/connection');
 
-const seedCategories = () => Category.bulkCreate(categoryData);
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log('\n----- DATABASE SYNCED -----\n');
+  await seedCategories();
+  console.log('\n----- CATEGORIES SEEDED -----\n');
 
-module.exports = seedCategories;
+  await seedProducts();
+  console.log('\n----- PRODUCTS SEEDED -----\n');
+
+  await seedTags();
+  console.log('\n----- TAGS SEEDED -----\n');
+
+  await seedProductTags();
+  console.log('\n----- PRODUCT TAGS SEEDED -----\n');
+
+  process.exit(0);
+};
+
+seedAll();
